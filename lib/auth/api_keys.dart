e4473c9af4897f9122251742119b447e37861625
Ty Gross <31942911+tygross025@users.noreply.github.com>
2022-09-23 15:11:43 +0200

    Update api_keys.dart
    removed api keys

--- lib/auth/api_keys.dart
+++ lib/auth/api_keys.dart
@@ -1,2 +1,2 @@
-const String dbClientId = '58a3b6f4562c4b0221bc2025a31cd5d1';
-const String dbApiKey = '31bf5d48ff7a4c8cfa263633e26d2418';
+const String dbClientId = '';
+const String dbApiKey = '';
